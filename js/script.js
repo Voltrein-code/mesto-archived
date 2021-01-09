2a59472df4f0c383d7555282454aae6d22c47d12
Voltrein-code <mr-voltrein@yandex.ru>
2021-01-09 12:38:44 +0300

    feat: добавить логику для попап

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 //Объявление переменных
 let page = document.querySelector('.page'); //видимая область страницы
-let closeButton = page.querySelector('.popup__close_action_reset'); //кнопка закрытия popup окна
-let editButton = page.querySelector('.profile__edit-button');
+let closeButton = page.querySelector('.popup__close'); //кнопка закрытия popup окна
+let editButton = page.querySelector('.profile__edit-button'); //кнопка редактирования профиля
 let popup = page.querySelector('.popup'); //область popup
-let popupOpened = 'popup_opened';
+let popupOpened = 'popup_opened'; //переменная с названием класса
+let profileName = page.querySelector('.profile__name'); //переменная с именем пользователя
+let profileCaption = page.querySelector('.profile__caption'); //переменная с описанием профиля
+let popupName = popup.querySelector('.popup__text_type_name'); //переменная со значением имени из попап
+let popupCaption = popup.querySelector('.popup__text_type_caption'); //переменная со значением описания из попап
 
 //Функция закрытия popup окна
 function closePopup() {
   popup.classList.remove(popupOpened);
+  page.style.overflow = 'auto';
 }
 
 //Функция открытия popup окна
 function openPopup() {
   popup.classList.add(popupOpened);
+  popupName.value = profileName.textContent;
+  popupCaption.value = profileCaption.textContent;
+  page.style.overflow = 'hidden';
 }
 
+function savePopup(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileCaption.textContent = popupCaption.value;
+  closePopup();
+}
+
+
 //События
-closeButton.addEventListener('click', closePopup);
 editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+popup.addEventListener('submit', savePopup);
